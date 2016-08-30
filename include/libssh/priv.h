e12c32bbe9cc2e3e9a07b271fe186429a20b0b08
Tilo Eckert <tilo.eckert@flam.de>
2016-08-30 12:54:04 +0200

    use custom strtoull() function if no system-version is available (e.g. on Win2000)

--- include/libssh/priv.h
+++ include/libssh/priv.h
@@ -32,15 +32,118 @@
 #include "config.h"
 
 #if !defined(HAVE_STRTOULL)
-# if defined(HAVE___STRTOULL)
-#  define strtoull __strtoull
-# elif defined(HAVE__STRTOUI64)
-#  define strtoull _strtoui64
-# elif defined(__hpux) && defined(__LP64__)
-#  define strtoull strtoul
-# else
-#  error "no strtoull function found"
-# endif
+#define strtoull my_strtoull
+#include "stdint.h"
+#include "ctype.h"
+#include "errno.h"
+#include "limits.h"
+
+// custom strtoull() implementation for cases where it is not available (e.g. older Windows versions)
+static uint64_t my_strtoull(const char *nptr, char **endptr, int base) {
+    uint64_t        result = 0;
+    const char     *p;
+    const char     *first_nonspace;
+    const char     *digits_start;
+    int             sign = 1;
+    int             out_of_range = 0;
+
+    if (base != 0 && (base < 2 || base > 36))
+        goto invalid_input;
+
+    p = nptr;
+
+    /*
+     * Process the initial, possibly empty, sequence of white-space characters.
+     */
+    while (isspace((unsigned char) (*p)))
+        p++;
+
+    first_nonspace = p;
+
+    /*
+     * Determine sign.
+     */
+    if (*p == '+')
+        p++;
+    else if (*p == '-') {
+        p++;
+        sign = -1;
+    }
+
+    if (base == 0) {
+        /*
+         * Determine base.
+         */
+        if (*p == '0') {
+            if ((p[1] == 'x' || p[1] == 'X')) {
+                if (isxdigit((unsigned char)(p[2]))) {
+                    base = 16;
+                    p += 2;
+                } else {
+                    /*
+                     * Special case: treat the string "0x" without any further
+                     * hex digits as a decimal number.
+                     */
+                    base = 10;
+                }
+            } else {
+                base = 8;
+                p++;
+            }
+        } else {
+            base = 10;
+        }
+    } else if (base == 16) {
+        /*
+         * For base 16, skip the optional "0x" / "0X" prefix.
+         */
+        if (*p == '0' && (p[1] == 'x' || p[1] == 'X')
+            && isxdigit((unsigned char)(p[2]))) {
+            p += 2;
+        }
+    }
+
+    digits_start = p;
+
+    for (; *p; p++) {
+        int             digit;
+        digit = ('0' <= *p && *p <= '9') ? *p - '0'
+            : ('a' <= *p && *p <= 'z') ? (*p - 'a' + 10)
+            : ('A' <= *p && *p <= 'Z') ? (*p - 'A' + 10) : 36;
+        if (digit < base) {
+            if (! out_of_range) {
+                if (result > ULLONG_MAX / base
+                    || result * base > ULLONG_MAX - digit) {
+                    out_of_range = 1;
+                }
+                result = result * base + digit;
+            }
+        } else
+            break;
+    }
+
+    if (p > first_nonspace && p == digits_start)
+        goto invalid_input;
+
+    if (p == first_nonspace)
+        p = nptr;
+
+    if (endptr)
+        *endptr = (char *) p;
+
+    if (out_of_range) {
+        errno = ERANGE;
+        return ULLONG_MAX;
+    }
+
+    return sign > 0 ? result : -result;
+
+  invalid_input:
+    errno = EINVAL;
+    if (endptr)
+        *endptr = (char *) nptr;
+    return 0;
+}
 #endif /* !defined(HAVE_STRTOULL) */
 
 #ifdef HAVE_BYTESWAP_H
